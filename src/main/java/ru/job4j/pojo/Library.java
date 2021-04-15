b8366f8dea995290f6f475d960833f33f58da6b3
Semyon Stanovov <sestanovov@gmail.com>
2021-04-15 17:44:11 +0300

    3. Массивы и модели. [#510813]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,29 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book[] books = new Book[4];
+        books[0] = new Book("Clean Code", 464);
+        books[1] = new Book("Effective Java", 464);
+        books[2] = new Book("Thinking In Java", 1168);
+        books[3] = new Book("Head First Design Patterns", 676);
+        for (int i = 0; i < books.length; i++) {
+            System.out.println(books[i].getName() + " - " + books[i].getCount());
+        }
+        System.out.println();
+        System.out.println("Replace indexes 0 and 3.");
+        Book tmp = books[0];
+        books[0] = books[3];
+        books[3] = tmp;
+        for (int i = 0; i < books.length; i++) {
+            System.out.println(books[i].getName() + " - " + books[i].getCount());
+        }
+        System.out.println();
+        System.out.println("Only with a name \"Clean Code\"");
+        for (int i = 0; i < books.length; i++) {
+            if (books[i].getName().equals("Clean Code")) {
+                System.out.println(books[i].getName() + " - " + books[i].getCount());
+            }
+        }
+    }
+}
