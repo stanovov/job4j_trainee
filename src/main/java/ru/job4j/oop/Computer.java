fa8c2c1f44f9a1fedcade58bcdef3d2135a279e9
Semyon Stanovov <sestanovov@gmail.com>
2021-04-15 14:09:04 +0300

    2. Перегрузка конструктора [#510805] Lecture

--- src/main/java/ru/job4j/oop/Computer.java
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -8,9 +8,20 @@ public class Computer {
     public Computer() {
     }
 
+    public Computer(int ssd, String cpu) {
+        this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
     public Computer(boolean multiMonitor, int ssd, String cpu) {
         this.multiMonitor = multiMonitor;
         this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
+    public Computer(boolean multiMonitor, double ssd, String cpu) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = (int) ssd;
         this.cpu = cpu;
     }
 
@@ -18,12 +29,17 @@ public class Computer {
         System.out.println("Много мониторов: " + multiMonitor);
         System.out.println("SSD: " + ssd + " GB");
         System.out.println("Модель CPU: " + cpu);
+        System.out.println();
     }
 
     public static void main(String[] args) {
-        Computer computer = new Computer(true, 500, "Intel Core I7-10700K");
-        computer.printInfo();
-        Computer comp = new Computer(true, 256, "AMD Ryzen 5 3600");
-        comp.printInfo();
+        Computer first = new Computer();
+        first.printInfo();
+        Computer second  = new Computer(true, 500, "Intel Core I7-10700K");
+        second .printInfo();
+        Computer third = new Computer(256, "AMD Ryzen 5 3600");
+        third.printInfo();
+        Computer fourth = new Computer(true, 512.0, "AMD Ryzen 7 3700X");
+        fourth.printInfo();
     }
 }
