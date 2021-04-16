a2d350988d9e9d551222e5ddcce410eb8795a32b
Semyon Stanovov <sestanovov@gmail.com>
2021-04-16 11:48:51 +0300

    6. Метод замены заявки. Tracker.replace [#510816]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -13,6 +13,16 @@ public class Tracker {
         return item;
     }
 
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        if (index == -1) {
+            return false;
+        }
+        item.setId(id);
+        items[index] = item;
+        return true;
+    }
+
     public Item[] findAll() {
         Item[] itemsWithoutNull = new Item[items.length];
         int length = 0;
@@ -36,11 +46,15 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
