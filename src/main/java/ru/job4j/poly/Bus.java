d1d072607b070b11f422b02c300b2bee9b1109d9
Semyon Stanovov <sestanovov@gmail.com>
2021-04-16 17:57:19 +0300

    3. Интерфейс Input [#510737] Минимальная реализация

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,17 +3,23 @@ package ru.job4j.poly;
 public class Bus implements Transport, Vehicle {
     @Override
     public void go() {
-
+        System.out.println("Автобус едет");
     }
 
     @Override
     public void passengers(int number) {
-
+        System.out.println("Пассажиры в количестве \"" + number + "\" садятся в автобус");
     }
 
     @Override
     public float refuel(int quantity) {
-        return 0;
+        float price = 43.92f;
+        float sum = (float) (Math.round(price * quantity * 100.0) / 100.0);
+        System.out.println(
+                "Автобус заправляется на " + quantity + " л.\n"
+                + "Стоимость заправки составляет - " + sum + " руб."
+        );
+        return sum;
     }
 
     @Override
