6c499d6c23af411f6615a751bb841976ced446d4
Semyon Stanovov <sestanovov@gmail.com>
2021-04-19 13:24:33 +0300

    8. Реализация меню за счет шаблона стратегия. [#510743]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -10,7 +10,7 @@ public class StartUITest {
         String[] answers = {"Fix PC"};
         Input input = new StubInput(answers);
         Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
+        new CreateAction().execute(input, tracker);
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
@@ -25,7 +25,7 @@ public class StartUITest {
                 String.valueOf(item.getId()),
                 "replaced item"
         };
-        StartUI.replaceItem(new StubInput(answers), tracker);
+        new ReplaceAction().execute(new StubInput(answers), tracker);
         Item replaced = tracker.findById(item.getId());
         assertThat(replaced.getName(), is("replaced item"));
     }
@@ -36,7 +36,7 @@ public class StartUITest {
         Item item = new Item("test item");
         tracker.add(item);
         String[] answers = {String.valueOf(item.getId())};
-        StartUI.deleteItem(new StubInput(answers), tracker);
+        new DeleteAction().execute(new StubInput(answers), tracker);
         assertNull(tracker.findById(item.getId()));
     }
 }
