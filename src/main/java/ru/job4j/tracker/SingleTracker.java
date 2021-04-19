d5e6d8bf4f2e86499366bc8b38d9a5cedab6ae72
Semyon Stanovov <sestanovov@gmail.com>
2021-04-19 19:29:20 +0300

    2. Singleton [#510778]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,39 @@
+package ru.job4j.tracker;
+
+public class SingleTracker {
+    private static Tracker instance = null;
+
+    private SingleTracker() {
+    }
+
+    public static Tracker getInstance() {
+        if (instance == null) {
+            instance = new Tracker();
+        }
+        return instance;
+    }
+
+    public Item add(Item item) {
+        return instance.add(item);
+    }
+
+    public boolean replace(int id, Item item) {
+        return instance.replace(id, item);
+    }
+
+    public boolean delete(int id) {
+        return instance.delete(id);
+    }
+
+    public Item[] findAll() {
+        return instance.findAll();
+    }
+
+    public Item[] findByName(String key) {
+        return instance.findByName(key);
+    }
+
+    public Item findById(int id) {
+        return instance.findById(id);
+    }
+}
