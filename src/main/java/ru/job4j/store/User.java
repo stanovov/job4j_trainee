1af9c3e49ddfe65c6f8483cc29ac1e421b141946
Semyon Stanovov <sestanovov@gmail.com>
2021-04-20 11:20:38 +0300

    Классы не относящиеся к трекеру убираю в отдельный пакет

--- src/main/java/ru/job4j/store/User.java
+++ src/main/java/ru/job4j/store/User.java
@@ -1,4 +1,4 @@
-package ru.job4j.tracker;
+package ru.job4j.store;
 
 public class User {
     private int id;
--- src/main/java/ru/job4j/store/UserStore.java
+++ src/main/java/ru/job4j/store/UserStore.java
@@ -1,4 +1,4 @@
-package ru.job4j.tracker;
+package ru.job4j.store;
 
 public class UserStore {
     public static void main(String[] args) {
