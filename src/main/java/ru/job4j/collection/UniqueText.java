5f6fc52713f4603f41fb857f4a4e1ff7914bd284
Semyon Stanovov <sestanovov@gmail.com>
2021-04-26 17:49:09 +0300

    2. Идентичные тексты [#510728]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String str : origin) {
+            check.add(str);
+        }
+        for (String str : text) {
+            if (!check.contains(str)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
