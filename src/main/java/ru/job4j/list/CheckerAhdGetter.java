11fd32c50b0da04437a7450c59a13edf684e277e
Semyon Stanovov <sestanovov@gmail.com>
2021-04-26 21:58:02 +0300

    1. Проверить список и получить первый элемент.

---
+++ src/main/java/ru/job4j/list/CheckerAhdGetter.java
@@ -0,0 +1,9 @@
+package ru.job4j.list;
+
+import java.util.List;
+
+public class CheckerAhdGetter {
+    public static String getElement(List<String> list) {
+        return list.isEmpty() ? "" : list.get(0);
+    }
+}
