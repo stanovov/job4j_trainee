4297f80f11bfbc374124619a346963239033b55b
Semyon Stanovov <sestanovov@gmail.com>
2021-04-27 11:25:06 +0300

    2. Идентичные тексты [#510728] Исправление

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -4,17 +4,19 @@ import java.util.HashSet;
 
 public class UniqueText {
     public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
         String[] origin = originText.split(" ");
         String[] text = duplicateText.split(" ");
         HashSet<String> check = new HashSet<>();
         for (String str : origin) {
-            check.add(str);
+            check.add(str.toLowerCase());
         }
         for (String str : text) {
-            if (!check.contains(str)) {
-                return false;
+            if (!check.contains(str.toLowerCase())) {
+                rsl = false;
+                break;
             }
         }
-        return true;
+        return rsl;
     }
 }
