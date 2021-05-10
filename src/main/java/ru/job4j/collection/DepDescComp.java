d72253a3f9e3eb6e6db2d7a1734bf3b374d4d8df
Semyon Stanovov <sestanovov@gmail.com>
2021-05-10 15:57:23 +0300

    2. Отсортировать департаменты [#510704] Исправил нагроможденность

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -7,25 +7,7 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String left, String right) {
         String[] leftArr = left.split("/");
         String[] rightArr = right.split("/");
-        int rsl = 0;
-        int len = Math.max(leftArr.length, rightArr.length);
-        for (int i = 0; i < len; i++) {
-            if (i < leftArr.length && i < rightArr.length) {
-                rsl = leftArr[i].compareTo(rightArr[i]);
-                if (i == 0) {
-                    rsl = -rsl;
-                }
-                if (rsl != 0) {
-                    break;
-                }
-            } else if (i == rightArr.length) {
-                rsl = 1;
-                break;
-            } else if (i == leftArr.length) {
-                rsl = -1;
-                break;
-            }
-        }
-        return rsl;
+        int rsl = rightArr[0].compareTo(leftArr[0]);
+        return rsl == 0 ? left.compareTo(right) : rsl;
     }
 }
