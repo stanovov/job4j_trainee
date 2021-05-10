4a51a192c9a2dc3b3c79b67a2fc8630b11d18b35
Semyon Stanovov <sestanovov@gmail.com>
2021-05-10 12:18:19 +0300

    1. Генератор текста (2)

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -6,8 +6,15 @@ import java.util.Set;
 
 public class Article {
     public static boolean generateBy(String origin, String line) {
-        Set<String> originSet = new HashSet<>(Arrays.asList(origin.split("[^A-zА-я]+")));
-        Set<String> lineSet = new HashSet<>(Arrays.asList(line.split("[^A-zА-я]+")));
-        return originSet.containsAll(lineSet);
+        Set<String> originSet = new HashSet<>(
+                Arrays.asList(origin.replaceAll("\\p{P}", "").split(" "))
+        );
+        String[] array = line.split(" ");
+        for (String str : array) {
+            if (!originSet.contains(str)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
