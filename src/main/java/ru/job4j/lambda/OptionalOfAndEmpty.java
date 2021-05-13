38e1774851f9f0889498d965c0f88e9ed2824399
Semyon Stanovov <sestanovov@gmail.com>
2021-05-13 17:50:44 +0300

    5.1. Optional. Методы of() и empty()

---
+++ src/main/java/ru/job4j/lambda/OptionalOfAndEmpty.java
@@ -0,0 +1,15 @@
+package ru.job4j.lambda;
+
+import java.util.List;
+import java.util.Optional;
+
+public class OptionalOfAndEmpty {
+    public static Optional<String> findValue(List<String> strings, String value) {
+        for (String s : strings) {
+            if (s.equals(value)) {
+                return Optional.of(s);
+            }
+        }
+        return Optional.empty();
+    }
+}
