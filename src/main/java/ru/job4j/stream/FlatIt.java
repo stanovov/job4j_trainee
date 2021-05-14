8ad3678b703bdbcfcc7815d3d5e57d236c62bc9a
Semyon Stanovov <sestanovov@gmail.com>
2021-05-14 13:58:42 +0300

    1. flatMap для итератора. New try

--- src/main/java/ru/job4j/stream/FlatIt.java
+++ src/main/java/ru/job4j/stream/FlatIt.java
@@ -2,6 +2,7 @@ package ru.job4j.stream;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public class FlatIt {
@@ -13,5 +14,8 @@ public class FlatIt {
                         Spliterators.spliteratorUnknownSize(its, characteristics), false)
                 )
                 .collect(Collectors.toList());
+//        return Stream.iterate(it.next(), i -> it.hasNext(), i -> it.next())
+//                .flatMap(its -> Stream.iterate(its.next(), i -> its.hasNext(), i -> its.next()))
+//                .collect(Collectors.toList());
     }
 }
